5ca5c81daf48635f5f9ed9aef469d995ace0fb36
Tobias Kunze <r@rixx.de>
2020-01-12 21:06:21 +0100

    Update zammad-notify from source

--- zammad-notify/zammad_notify.py
+++ zammad-notify/zammad_notify.py
@@ -46,6 +46,8 @@ def limit_length(text, length):
 def send_notification(notification):
     ticket_id = notification["o_id"]
     ticket = zammad_get(f"/api/v1/ticket_articles/by_ticket/{ticket_id}")[-1]
+    if any((ticket.get(text) or "").startswith("RT @") for text in ["subject", "body"]):
+        return
     ticket_data = zammad_get(f"/api/v1/tickets/{ticket_id}")
     customer = zammad_get(f'/api/v1/users/{ticket_data["customer_id"]}')
     name = customer.get("firstname", " ") + " " + customer.get("lastname", "")
@@ -59,7 +61,7 @@ def send_notification(notification):
         "user": config["pushover"]["user"],
         "title": limit_length(f"{ticket['subject']} ({name})", 250),
         "message": limit_length(ticket["body"], 1024),
-        "url": config["zammad"]["url"] + f"/#ticket/zoom/{ticket_id}",
+        "url": config["zammad"]["url"] + f'/#ticket/zoom/{ticket_id}',
         "url_title": "Go to ticket",
         "sound": "none",
     }
