8dcf9a781aef3011b0cd9be0b06fde95dd758e07
Tobias Kunze <r@rixx.de>
2020-05-02 18:04:42 +0200

    Sadpanda

---
+++ spamsigh/spamsigh.py
@@ -0,0 +1,16 @@
+import base64
+import html
+import subprocess
+
+with open("/home/rixx/tmp/downloads/original message before SpamAssassin.txt") as f: 
+    content = f.read().split("\n")
+
+encoding = 'Content-Transfer-Encoding: base64'
+index = content.index(encoding)
+content = base64.decodebytes((content[index + 2]).encode()).decode()
+link_index = content.find("a href")
+link_start = content[link_index + 8:]
+url = link_start[:link_start.find('"')]
+url = html.unescape(url)
+# url = url.replace("&amp;", "&")
+subprocess.call(["xdg-open", url])
