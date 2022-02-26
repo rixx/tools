6d2b87d5a79d8dcaa3d597a964e4c026b5b84cfb
Tobias Kunze <r@rixx.de>
2022-02-26 15:47:31 +0100

    [speedrun-race-timer] Add generator for more runners

--- speedrun-race-timer/generate_splits.py
+++ speedrun-race-timer/generate_splits.py
@@ -3,12 +3,17 @@ MYLA = ["Mound", "Greenpath", "Dash", "Claw", "Blue Lake", "Sly", "Cdash"]
 SPLITS = MYLA
 
 
+
+SPLITS = TE_SCSBCP
+RUNNERS = 2
+
+runner_lines = '          <td><input type="text" /></td>\n' * RUNNERS
+runner_headers = '          <th contenteditable>R1 (editme)</th>' * RUNNERS
 result = "\n".join(
     [
         f"""        <tr>
           <td>{split}</td>
-          <td><input type="text" /></td>
-          <td><input type="text" /></td>
+          {runner_lines}
           <td></td>
           <td></td>
         </tr>"""
@@ -20,6 +25,7 @@ with open("index.html", "r") as fp:
     content = fp.read()
 
 content = content.replace("#PLACEHOLDER", result)
+content = content.replace("#RUNNERS", runner_headers)
 
 with open("index.html", "w") as fp:
     fp.write(content)
