20ba1bf52bc8f3fce347011c0b4766d30c96874f
Tobias Kunze <r@rixx.de>
2022-12-03 10:40:50 +0100

    [tatort] Handle download given a URL

--- tatort/manage.py
+++ tatort/manage.py
@@ -1,15 +1,21 @@
 import csv
+import re
+import os
+import subprocess
+import sys
+from pathlib import Path
 
 import bs4
+import inquirer
 import requests
 
 
-response = requests.get("https://de.wikipedia.org/wiki/Liste_der_Tatort-Folgen")
-content = bs4.BeautifulSoup(response.content.decode(), "html.parser")
-table = content.find("table")
+CWD = Path(os.getcwd())
+CSV_PATH = Path(__file__).parent / "episodes.csv"
+EPISODES = []
 
 
-def get_episode(line):
+def serialize_episode(line):
     fields = line.findAll("td")
     if not fields:
         return
@@ -26,25 +32,118 @@ def get_episode(line):
     }
 
 
-episodes = [get_episode(line) for line in table.findAll("tr")]
-episodes = [e for e in episodes if e]
+def update_csv():
+    print("Fetching data from Wikipedia")
+    response = requests.get("https://de.wikipedia.org/wiki/Liste_der_Tatort-Folgen")
+    content = bs4.BeautifulSoup(response.content.decode(), "html.parser")
+    table = content.find("table")
+    episodes = []
+    episodes = [serialize_episode(line) for line in table.findAll("tr")]
+    episodes = [e for e in episodes if e]
 
-print(f"{len(episodes)} Episoden gefunden!")
+    print(f"{len(episodes)} Episoden gefunden!")
 
-with open("episodes.csv", "w") as fp:
-    writer = csv.DictWriter(
-        fp,
-        fieldnames=[
-            "episode",
-            "titel",
-            "datum",
-            "ermittler",
-            "ermittler_episode",
-            "wiki_link",
-            "ermittler_link",
-            "sender",
-            "kommentar",
-        ],
-    )
-    writer.writeheader()
-    writer.writerows(episodes)
+    with open(CSV_PATH, "w") as fp:
+        writer = csv.DictWriter(
+            fp,
+            fieldnames=[
+                "episode",
+                "titel",
+                "datum",
+                "ermittler",
+                "ermittler_episode",
+                "wiki_link",
+                "ermittler_link",
+                "sender",
+                "kommentar",
+            ],
+        )
+        writer.writeheader()
+        writer.writerows(episodes)
+
+
+def slugify(s):
+    return re.sub(r"[\W_]+", "-", s.lower())
+
+
+def load_csv():
+    if not CSV_PATH.exists():
+        update_csv()
+    with open(CSV_PATH, "r") as fp:
+        reader = csv.DictReader(fp)
+        data = [l for l in reader]
+    for entry in data:
+        entry["slug"] = slugify(entry["titel"])
+    return data
+
+
+def get_episode(url):
+    global EPISODES
+    if not EPISODES:
+        EPISODES = load_csv()
+    response = requests.get(url)
+    content = bs4.BeautifulSoup(response.content.decode(), "html.parser")
+    title = content.find("meta", {"property": "og:title"}).attrs["content"]
+    if "(" in title:
+        title = title[: title.find("(")]
+    if "Tatort:" in title:
+        title = title[title.find("Tatort:") + len("Tatort:") :]
+    slug = slugify(title.strip())
+    matches = [
+        e for e in EPISODES if e["slug"].startswith(slug) or slug.startswith(e["slug"])
+    ]
+    if not matches:
+        print("Episode not found! Can't assign file name, aborting")
+        return
+    if len(matches) == 1:
+        result = matches[0]
+    else:
+        result = inquirer.list_input(
+            "Which Episode is the right one?",
+            [(f"{e['episode']} – {e['title']}", e) for e in matches]
+            + [("None, abort", None)],
+            carousel=True,
+        )
+    result["filename"] = slug
+    return result
+
+
+def handle_download(url):
+    episode = get_episode(url)
+    if not episode:
+        return
+    number = f"{int(episode['episode']):04d}"
+    found = list(CWD.glob(f"{number}-*"))
+    if found:
+        print(f"Episode exists on disk: {found[0]}")
+        return
+    filename = f"{number}-{episode['filename']}.mp4"
+    print(f"Downloading episode {number}: {episode}['titel'] to {filename}")
+    subprocess.call(["youtube-dl", "-o", filename, url])
+
+
+def download():
+    print("Welcome to the Tatort downloader.")
+    global EPISODES
+    EPISODES = load_csv()
+    if len(sys.argv) > 2:
+        for url in sys.argv[2:]:
+            handle_download(url)
+    else:
+        while True:
+            url = inquirer.text("Enter a URL (or q[uit] to quit)")
+            if url in ("q", "quit"):
+                break
+            handle_download(url)
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    if arg == "update_csv":
+        update_csv()
+    elif arg == "download":
+        download()
+    else:
+        print(
+            "Call script with either 'update_csv' or 'download' (with a link or without to enter interactive mode)"
+        )
