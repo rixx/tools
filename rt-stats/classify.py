05114028ca6372728e004688cbc43942519c68fc
Tobias Kunze <r@rixx.de>
2023-11-10 17:41:35 +0100

    [rt-stats] Add stats

--- rt-stats/classify.py
+++ rt-stats/classify.py
@@ -1,24 +1,48 @@
+import datetime as dt
 import json
 import pathlib
 import sys
+from collections import defaultdict
 
 import click
 import httpx
 import rt.rest2
+from tqdm import tqdm
+
+
+def get_time(time_str):
+    return dt.datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ")
+
+
+def format_delta(delta):
+    days = delta.days
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    result = ""
+    if days:
+        result += f"{days}d "
+    result += f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+    return result
+
+
+def print_leaderboard(data, title):
+    total = sum(data.values())
+    print(f"#### {title}:")
+    for user, count in sorted(data.items(), key=lambda x: x[1], reverse=True):
+        percent = count / total * 100
+        # align users to 20 chars, counts to 5 chars and percent to 5 chars
+        print(f"  {user:20} {count:5} ({percent:5.1f}%)")
+    print()
 
 
 def get_tracker(auth_data):
-    tracker = rt.rest2.Rt(
+    return rt.rest2.Rt(
         url=auth_data["url"],
         http_auth=httpx.BasicAuth(
             auth_data["username"],
             auth_data["password"],
         ),
     )
-    if not tracker.get_all_queues():
-        click.echo("Error logging in!")
-        sys.exit(-1)
-    return tracker
 
 
 @click.group()
@@ -48,11 +72,103 @@ def auth(auth):
     user = click.prompt("Username")
     password = click.prompt("Password")
     auth_data = {"url": url, "username": user, "password": password}
-    get_tracker(auth_data)
+    tracker = get_tracker(auth_data)
+    if not tracker.get_all_queues():
+        click.echo("Error logging in!")
+        sys.exit(-1)
     open(auth, "w").write(json.dumps(auth_data, indent=4) + "\n")
     click.echo()
     click.echo(f"Your authentication credentials have been saved to {auth}.")
     click.echo()
+
+
+@cli.command()
+@click.option(
+    "-a",
+    "--auth",
+    type=click.Path(file_okay=True, dir_okay=False, allow_dash=False),
+    default="auth.json",
+    help="Path to auth data, defaults to ./auth.json.",
+)
+@click.argument("queue")
+@click.option(
+    "--ignore-users",
+    type=click.STRING,
+    help="Ignore tickets created by these users",
+    default="",
+)
+def stats(queue, auth, ignore_users):
+    auth = json.load(open(auth))
+    rt = get_tracker(auth)
+    queue = rt.get_queue(queue)
+    tickets = rt.search(queue=queue["Name"])
+    ignore_users = set(ignore_users.split(","))
+    ignore_users.add("RT_System")
+
+    total = 0
+    action_types = defaultdict(int)
+    actions_by_user = defaultdict(int)
+    replies_by_user = defaultdict(int)
+    time_first_reply = []
+    unknown_types = set()
+
+    for ticket in tqdm(tickets, desc="Tickets", total=float("inf")):
+        total += 1
+        history = rt.get_ticket_history(ticket["id"])
+        response_time = None
+        for transaction in history:
+            if (
+                "@" in transaction["Creator"]["Name"]
+                or transaction["Creator"]["Name"] in ignore_users
+            ):
+                continue
+            if transaction["Type"] in (
+                "AddReminder",
+                "AddWatcher",
+                "Create",
+                "DelWatcher",
+                "SetWatcher",
+            ):
+                continue
+            action_types[transaction["Type"]] += 1
+            if transaction["Type"] in (
+                "AddLink",
+                "Comment",
+                "Correspond",
+                "Set",
+                "Status",
+            ):
+                actions_by_user[transaction["Creator"]["Name"]] += 1
+            else:
+                unknown_types.add(transaction["Type"])
+                continue
+            if transaction["Type"] == "Correspond":
+                replies_by_user[transaction["Creator"]["Name"]] += 1
+                if not response_time:
+                    response_time = get_time(transaction["Created"]) - get_time(
+                        ticket["Created"]
+                    )
+        if response_time and response_time < dt.timedelta(days=30):
+            # we never take longer than a month to reply, this is weird data
+            time_first_reply.append(response_time)
+
+    print(f"\n\n#### {queue['Name']}")
+    print(f"Found {total} tickets in queue {queue['Name']}.\n")
+    print_leaderboard(actions_by_user, "Actions")
+    print_leaderboard(replies_by_user, "Replies")
+
+    avg_response_time = sum(time_first_reply, dt.timedelta()) / len(time_first_reply)
+    min_response_time = min(time_first_reply)
+    max_response_time = max(time_first_reply)
+    print(
+        f"Average response time: {format_delta(avg_response_time)}, "
+        f"min: {format_delta(min_response_time)}, "
+        f"max: {format_delta(max_response_time)}\n"
+    )
+
+    print_leaderboard(action_types, "Action types")
+    if unknown_types:
+        print(f"Unknown types: {unknown_types}")
 
 
 if __name__ == "__main__":
