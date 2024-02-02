9f896861d9b7be8cd6fdc45f1243d557d8f01904
Tobias Kunze <r@rixx.de>
2024-02-02 08:32:41 +0100

    [zammad_notify] Restructure into class

--- zammad-notify/zammad_notify.py
+++ zammad-notify/zammad_notify.py
@@ -22,7 +22,59 @@ SUBJECT_BLOCKED = [
     "auto reply",
     "away from work",
     "out of office",
+    "rt @",
 ]
+
+
+class Ticket:
+    def __init__(self, ticket_id):
+        self.ticket_id = ticket_id
+        ticket = zammad_get(f"/api/v1/ticket_articles/by_ticket/{self.ticket_id}")[-1]
+        ticket_data = zammad_get(f"/api/v1/tickets/{self.ticket_id}")
+        customer_data = zammad_get(f'/api/v1/users/{ticket_data["customer_id"]}')
+        self.subject = ticket_data["title"]
+        self.customer_name = (
+            customer_data["firstname"] + " " + customer_data["lastname"]
+        ).strip()
+        self.customer_email = customer_data["email"]
+        body = ticket["body"]
+        if "<" in body:
+            try:
+                body = BeautifulSoup(body, "html.parser").text.strip()
+            except Exception:
+                pass
+        self.body = body
+
+    def should_notify(self):
+        if not self.body:
+            return False
+        for blocked in SUBJECT_BLOCKED:
+            if blocked in self.subject.lower():
+                return False
+        for blocked in BODY_BLOCKED:
+            if blocked in self.body.lower():
+                return False
+
+    def send_notification(self, force=False):
+        if not self.should_notify() and not force:
+            return
+        name = self.customer_name or f"<{self.customer_email}>"
+
+        payload = {
+            "token": config["pushover"]["app"],
+            "user": config["pushover"]["user"],
+            "title": limit_length(f"{self.subject} ({name})", 250),
+            "message": limit_length(self.body, 1024),
+            "url": f"{config['zammad']['url']}/#ticket/zoom/{self.ticket_id}",
+            "url_title": "Go to ticket",
+            "sound": "none",
+        }
+        response = requests.post(
+            url="https://api.pushover.net/1/messages.json",
+            data=json.dumps(payload),
+            headers={"Content-Type": "application/json"},
+        )
+        response.raise_for_status()
 
 
 def get_seen_ids():
@@ -63,65 +115,19 @@ def limit_length(text, length):
     return text[: length - 2] + "…"
 
 
-def send_notification(notification):
-    ticket_id = notification["o_id"]
-    ticket = zammad_get(f"/api/v1/ticket_articles/by_ticket/{ticket_id}")[-1]
-    if any((ticket.get(text) or "").startswith("RT @") for text in ["subject", "body"]):
-        return
-    ticket_data = zammad_get(f"/api/v1/tickets/{ticket_id}")
-    customer = zammad_get(f'/api/v1/users/{ticket_data["customer_id"]}')
-    subject = ticket_data["title"]
-    for blocked in SUBJECT_BLOCKED:
-        if blocked in subject.lower():
-            return
-    name = customer.get("firstname", " ") + " " + customer.get("lastname", "")
-    name = name.strip()
-    if not name:
-        name = f'<{customer["email"]}>'
-    body = ticket["body"]
-    for blocked in BODY_BLOCKED:
-        if blocked in body:
-            return
-    if "<" in body:
-        try:
-            body = BeautifulSoup(body, "html.parser").text.strip()
-        except Exception:
-            pass
-    if not body:
-        return
-    auth = ""
-    if "http_user" in config["zammad"]:
-        auth = f"{config['zammad']['http_user']}:{config['zammad']['http_pass']}@"
-
-    payload = {
-        "token": config["pushover"]["app"],
-        "user": config["pushover"]["user"],
-        "title": limit_length(f"{subject} ({name})", 250),
-        "message": limit_length(body, 1024),
-        "url": f"{auth}{config['zammad']['url']}/#ticket/zoom/{ticket_id}",
-        "url_title": "Go to ticket",
-        "sound": "none",
-    }
-    response = requests.post(
-        url="https://api.pushover.net/1/messages.json",
-        data=json.dumps(payload),
-        headers={"Content-Type": "application/json"},
-    )
-    response.raise_for_status()
-
-
 def main():
     seen_ids = get_seen_ids()
     try:
         notifications = get_unread_notifications()
-    except Exception as e:
+    except Exception:
         # This usually happens when DNS is failing.
         # Fail quietly, as exceptions are just annoying, and this isn't critical.
         return
     new_seen_ids = []
     for notification in notifications:
         if notification["id"] not in seen_ids:
-            send_notification(notification)
+            ticket = Ticket(notification["o_id"])
+            ticket.send_notification()
         new_seen_ids.append(notification["id"])
     write_seen_ids(new_seen_ids)
 
